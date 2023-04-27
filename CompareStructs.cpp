68860a76bd5441ca1d860638009f98501fbc1933
Justin Smith <jtsmith7248@gmail.com>
2023-04-27 12:44:33 -0400

    Added CompareStructs.cpp

---
+++ CompareStructs.cpp
@@ -0,0 +1,28 @@
+// This file houses the struct CompareXXX templated functions
+
+// BubbleSort pulls in these Compare functions using a template
+
+struct CompareSuits
+{
+	bool Compare(DeckOfCards::DrawCard* c1, DrawCard::Photo* c2) {
+		// auto S1 = SetSuits(C1.suits)
+		// Same for S2
+		// if equivalent: use CompareNumbers() and return bool
+		// return bool comparison
+	}
+	int setSuit(string suit) {
+		// set the values of suit; maybe use switch function?
+		// case H: 4
+		// case D: 3
+		// case C: 2
+		// case S: 1
+	}
+};
+
+struct CompareSuits
+{
+	bool Compare(DeckOfCards::DrawCard* c1, DrawCard::Photo* c2) {
+		// Similar idea to Compare function in CompareSuits
+	}
+};
+
