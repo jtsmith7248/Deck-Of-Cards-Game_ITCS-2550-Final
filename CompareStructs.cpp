f744d57648a2c62033be0bd546f600ffe6c3de73
Sonya Tran <tranthuysongnguyen9911@gmail.com>
2023-04-29 02:55:21 -0400

    CompareSuitEnum, CompareSuits, and CompareValues added

--- CompareStructs.cpp
+++ CompareStructs.cpp
@@ -2,30 +2,119 @@
 
 // BubbleSort pulls in these Compare functions using a template
 
-struct CompareSuits
+// WITH ENUM
+struct CompareSuitEnum
 {
-	bool Compare(DeckOfCards::DrawCard* c1, DrawCard::Photo* c2) {
-		// auto S1 = SetSuits(C1.suits)
-		// Same for S2
-		// if equivalent: use CompareNumbers() and return bool
-		// return bool comparison
-	}
-	int setSuit(string suit) {
-		// set the values of suit; maybe use switch function?
-		// case H: 4
-		// case D: 3
-		// case C: 2
-		// case S: 1
-	}
+public:
+    bool Compare(DeckOfCards::DrawCard* c1, DeckOfCards::DrawCard* c2)
+    {
+        // Set the suits to numeric values
+        int suitNum1 = 0;
+        int suitNum2 = 0;
+
+        switch (c1->suit)
+        {
+        case "HEARTS":
+            suitNum1 = DeckOfCards::SUITS::HEARTS;
+            break;
+        case "DIAMONDS":
+            suitNum1 = DeckOfCards::SUITS::DIAMONDS;
+            break;
+        case "CLUBS":
+            suitNum1 = DeckOfCards::SUITS::CLUBS;
+            break;
+        case "SPADES":
+            suitNum1 = DeckOfCards::SUITS::SPADES;
+            break;
+        default:
+            suitNum1 = DeckOfCards::SUITS::NULLSUIT;
+            break;
+        }
+
+        switch (c2->suit)
+        {
+        case "HEARTS":
+            suitNum2 = DeckOfCards::SUITS::HEARTS;
+            break;
+        case "DIAMONDS":
+            suitNum2 = DeckOfCards::SUITS::DIAMONDS;
+            break;
+        case "CLUBS":
+            suitNum2 = DeckOfCards::SUITS::CLUBS;
+            break;
+        case "SPADES":
+            suitNum2 = DeckOfCards::SUITS::SPADES;
+            break;
+        default:
+            suitNum2 = DeckOfCards::SUITS::NULLSUIT;
+            break;
+        }
+
+        return suitNum1 > suitNum2;
+    }
 };
 
+// WITHOUT ENUM
 struct CompareSuits
 {
-	bool Compare(DeckOfCards::DrawCard* c1, DrawCard::Photo* c2) {
-		// Similar idea to Compare function in CompareSuits
-	}
+public:
+    bool Compare(DeckOfCards::DrawCard* c1, DeckOfCards::DrawCard* c2)
+    {
+        // Set the suits to numeric values
+        int suitNum1 = 0;
+        int suitNum2 = 0;
+
+        switch (c1->suit)
+        {
+        case "HEARTS":
+            suitNum1 = 4;
+            break;
+        case "DIAMONDS":
+            suitNum1 = 3;
+            break;
+        case "CLUBS":
+            suitNum1 = 2;
+            break;
+        case "SPADES":
+            suitNum1 = 1;
+            break;
+        default:
+            suitNum1 = 0;
+            break;
+        }
+
+        switch (c2->suit)
+        {
+        case "HEARTS":
+            suitNum2 = 4;
+            break;
+        case "DIAMONDS":
+            suitNum2 = 3;
+            break;
+        case "CLUBS":
+            suitNum2 = 2;
+            break;
+        case "SPADES":
+            suitNum2 = 1;
+            break;
+        default:
+            suitNum2 = 0;
+            break;
+        }
+
+        return suitNum1 > suitNum2;
+    }
 };
 
+// WITH/WITHOUT ENUM
+struct CompareValues
+{
+public:
+    bool Compare(DeckOfCards::DrawCard* c1, DeckOfCards::DrawCard* c2)
+    {
+        int value1 = c1->number;
+        int value2 = c2->number;
 
-
-
+        return value1 > value2;
+    }
+};
